908027236cad00041153f71c6a9a27e7cdcad13c
INFRA-DEV\i.gurieva <i.gurieva@mightycall.com>
2018-03-23 16:13:56 +0300

    AddContactTest

--- addressbook-web-tests/addressbook-web-tests/AccountData.cs
+++ addressbook-web-tests/addressbook-web-tests/AccountData.cs
@@ -6,7 +6,7 @@ using System.Threading.Tasks;
 
 namespace WebAddressbookTests
 {
-    class AccountData
+    public class AccountData
     {
         private string username;
         private string password;
@@ -39,5 +39,6 @@ namespace WebAddressbookTests
                 password = value;
             }
         }
+
     }
 }
---
+++ addressbook-web-tests/addressbook-web-tests/ContactData.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WebAddressbookTests
+{
+    class ContactData
+    {
+        private string firstName;
+        private string lastName;
+        /*
+        private string middleName;
+        private string nickName;
+        private string companyTitle;
+        private string companyAddress;
+        private string email;
+        private string mobilePhone;
+        private string homePhone;
+        */
+
+        public ContactData(string firstName, string lastName)
+        {
+            this.firstName = firstName;
+            this.lastName = lastName;
+        }
+
+        public string FirstName
+        {
+            get
+            {
+                return firstName;
+            }
+            set
+            {
+                firstName = value;
+            }
+        }
+
+        public string LastName
+        {
+            get
+            {
+                return lastName;
+            }
+            set
+            {
+                lastName = value;
+            }
+        }
+    }
+}
